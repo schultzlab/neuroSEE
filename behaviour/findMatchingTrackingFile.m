ae9eecb9c8328dd12d2af6dffa16a32c63b84abb
Simon Schultz <s.schultz@imperial.ac.uk>
2019-05-21 18:06:32 +0100

    Add files via upload

---
+++ behaviour/findMatchingTrackingFile.m
@@ -0,0 +1,144 @@
+% Written by Ann Go (adapted from Katie's identifyMouseExperimentDates.m)
+% 
+% This function finds the tracking file in dir_track that matches the 2P image 
+% INPUTS
+%   data_locn   : directory where GCaMP6 data is
+%   file        : part of file name of 2P image in the format
+%                   yyyymmdd_HH_MM_SS
+%   force       : ignores existing mat file for matching tracking file
+% OUTPUT
+%   fname_track : filename of tracking file matching 2P image
+
+function fname_track = findMatchingTrackingFile(data_locn, file, force)
+    if nargin<3, force = 0; end
+        
+    dir_track = [data_locn 'Data/' file(1:8) '/Neurotar/'];
+    dir_processed = [data_locn 'Data/' file(1:8) '/Processed/' file '/'];
+        if ~exist(dir_processed,'dir'), mkdir(dir_processed); end
+    
+    imgformat   = 'yyyymmdd_HH_MM_SS';
+    imgtime     = datevec(file,imgformat);
+    matchtype   = '[0-9a-zA-Z ]+'; % match dir & filenames based on alphanumeric chars (but not '_')
+    [tracktimes,trackformat]  = getTrackTimesFromDirectory( dir_track, matchtype );
+    finaltracktime = matchImgAndTrackTimes( imgtime, tracktimes );
+    trackfolder = [dir_track datestr( finaltracktime, trackformat, force )];
+    fname_track = findTrackFileName( trackfolder, dir_processed, force );
+
+    function [times,format] = getTrackTimesFromDirectory( ddir, matchtype )
+       times = [];  
+       if ~exist( ddir, 'dir' ) 
+          return;
+       end
+       timesdir  = dir( ddir );
+       format = 'Track_yyyy-mm-dd-HH-MM-SS'; 
+       trackregexp = genRegExpFromFormat( format, matchtype );
+       for i=1:length(timesdir)
+          timedir  = timesdir(i).name;
+          if ~isempty( regexp( timedir, trackregexp, 'once' ) )
+             time  = extractTimeFromFilename( timedir, format );
+             times = [times; time]; 
+          end
+       end
+    end
+
+    % generate a regular expression from date formats
+    function dateregexp = genRegExpFromFormat( format, matchtype )
+       if nargin<2, matchtype = '[0-9a-zA-Z\ ]+'; end
+       dateparts   = regexp( format, matchtype );
+       datesep     = unique( format( dateparts(2:end)-1 ) );
+
+       % if we seem to have more than 1 separator, we'll have to construct the
+       % format manually
+       if length( datesep ) > 1
+          % we start by expecting chars since most OSs require alpha char 1st 
+          dateregexp = matchtype;
+          for ii=1:length( format )
+             if any( format(ii)==datesep )
+                sep = ['[' datesep( format(ii)==datesep ) ']'];
+                dateregexp = [dateregexp  sep  matchtype]; 
+             end
+          end
+
+       % only 1 separator found so create a reg exp from it 
+       else
+          datesep     = ['[' datesep ']']; % require 1 separator btwn each component
+          % gen reg exp for date as characters separated by separator
+          dateregexp  = repmat( [matchtype datesep], [1 length(dateparts)] );
+          dateregexp  = dateregexp(1:end-length(datesep)); % remove final separator
+       end
+    end
+
+    function filetime = extractTimeFromFilename( timedir, format )
+       if length( timedir ) ~= length( format )
+          filetime = [];
+          return;
+       end
+       stind = @( sym ) strfind( format(1,:), sym ); % start index of date/time symbol
+       ind   = @( sym ) ternaryOp( isempty(stind(sym)), [], stind(sym):(stind(sym)+length(sym)-1) );
+
+       year = str2double( timedir( ind( 'yyyy' ) ) );
+       month= str2double( timedir( ind( 'mm' ) ) );
+       day  = str2double( timedir( ind( 'dd' ) ) );
+       hour = str2double( timedir( ind( 'HH' ) ) );
+       min  = str2double( timedir( ind( 'MM' ) ) );
+       sec  = str2double( timedir( ind( 'SS' ) ) );
+
+       filetime = [year month day hour min sec];
+    end
+
+    function finaltracktime = matchImgAndTrackTimes(imgtime, tracktimes)
+       maxtrackdelay = 10; % track recording must begin within this many mins of Ca image
+
+       % now go through tracking files, & find the files that
+       % are within a few minutes of imaging time
+       timediff = etime(tracktimes,imgtime);
+       valid_ind =  timediff>0 & timediff<=maxtrackdelay*60;
+       finaltracktime = tracktimes(timediff==min(timediff(valid_ind)),:);
+    end
+
+    function fname_track = findTrackFileName(trackfolder,dir_processed,force)
+        if nargin<2, force = 0; end
+        if force
+            csvfile = subdir(fullfile(trackfolder,['*.','csv'])); % Files from 2018
+            if numel(csvfile) == 1
+                fname_track = csvfile.name;
+            else 
+                tdmsfile = subdir(fullfile(trackfolder,['*.','tdms'])); % Files from 2019
+                if numel(tdmsfile) == 1
+                    fname_track = tdmsfile.name;
+                else
+                    fprintf('Tracking file does not exist in folder\n');
+                end
+            end
+        else
+            matfiles = subdir(fullfile(dir_processed,['*.','mat']));
+            if numel(matfiles) > 0 
+                s = zeros(1,numel(matfiles));
+                for i = 1:numel(matfiles)
+                    name = matfiles(i).name;
+                    if ~strcmpi(name(end-9:end-4),'output') && ~strcmpi(name(end-10:end-4),'allData')
+                        fname_track = matfiles(i).name;
+                        s(i) = 1;
+                    else 
+                        s(i) = 0;
+                    end
+                end
+            end
+                
+            if ~any(s)
+                csvfile = subdir(fullfile(trackfolder,['*.','csv'])); % Files from 2018
+                if numel(csvfile) == 1
+                    fname_track = csvfile.name;
+                else 
+                    tdmsfile = subdir(fullfile(trackfolder,['*.','tdms'])); % Files from 2019
+                    if numel(tdmsfile) == 1
+                        fname_track = tdmsfile.name;
+                    else
+                        fprintf('Tracking file does not exist in folder\n');
+                    end
+                end
+            end            
+        end
+    end
+
+end % function
---
+++ behaviour/load_trackfile.m
@@ -0,0 +1,82 @@
+% Written by Ann Go
+%
+% Loads the tracking file depending on its type
+% INPUTS
+%   fname_track : tracking file name
+% OUPUT
+% trackdata is a structure with fields
+%   x : mouse's x position                                (mm)
+%   y : mouse's y position                                (mm)
+%   r : mouse's radial position                           (mm)
+%   phi: mouse's angular position                         (degree)
+%   time : time from start of recording                   (s)
+%   speed : mouse speed                                   (mm/s)
+%   TTLout : TTL output signal
+%   alpha 
+%   w 
+
+function trackdata = load_trackfile(data_locn,file,fname_track,force)
+    str = sprintf('%s: Loading tracking data\n', file);
+    cprintf(str)
+    
+    dir_processed = [data_locn 'Data/' file(1:8) '/Processed/' file '/'];
+        if ~exist(dir_processed,'dir'), mkdir(dir_processed); end
+    
+    [~,~,ext] = fileparts(fname_track);
+    fname_fig = [dir_processed file '_mtrajectory.fig'];
+    fname_pdf = [dir_processed file '_mtrajectory.pdf'];
+    switch ext
+        case('.mat')
+            data = load(fname_track);
+            trackdata.time = data.time;
+            trackdata.r = data.r;
+            trackdata.phi = data.phi;
+            trackdata.alpha = data.alpha;
+            trackdata.x = data.x;
+            trackdata.y = data.y;
+            trackdata.w = data.w;
+            trackdata.TTLout = data.TTLout;
+            trackdata.speed = data.speed;
+        case('.csv') % 2018 files
+            save_fname = [dir_processed fname_track(end-33:end-4) '.mat'];
+            trackdata = csv_import(fname_track,save_fname);
+        case('.tdms') % 2019 files
+            
+            [channelData,~] = TDMS_readChannelOrGroup(fname_track,'Data',[]);
+            
+            reltime = datetime(channelData{2},'Format','HH:mm:ss.SSS');
+                [H,M,S] = hms(reltime(2:end));
+                time = S + M*60 + H*60*60;     trackdata.time   = time;
+
+            r      = channelData{3}(2:end);    trackdata.r      = r;
+            phi    = channelData{4}(2:end);    trackdata.phi    = phi;
+            alpha  = channelData{5}(2:end);    trackdata.alpha  = alpha;
+            x      = channelData{6}(2:end);    trackdata.x      = x;
+            y      = channelData{7}(2:end);    trackdata.y      = y;
+            w      = channelData{8}(2:end);    trackdata.w      = w;
+            speed  = channelData{9}(2:end);    trackdata.speed  = speed;
+            TTLout = channelData{12}(2:end);   trackdata.TTLout = TTLout;
+            try
+                save_fname = [dir_processed fname_track(end-34:end-5) '.mat'];
+                save(save_fname, 'time','r','phi','alpha','x','y','w','speed','TTLout');
+            catch
+                save_fname = [dir_processed fname_track(end-29:end-5) '.mat'];
+                save(save_fname, 'time','r','phi','alpha','x','y','w','speed','TTLout');
+            end
+    end
+    
+    yn_fname_fig = exist(fname_fig,'file');
+    yn_fname_pdf = exist(fname_pdf,'file');
+    if any([force,~yn_fname_fig,~yn_fname_pdf])
+        fig = figure; plot(trackdata.x,trackdata.y); axis square; axis off; 
+        tmax = (trackdata.time(end))/60; % min
+            titletext = [file(1:8) '-' file(10:11) '.' file(13:14) '.' file(16:17) ' (' num2str(round(tmax)) ' min)'];
+            title(titletext);
+        savefig(fig,fname_fig);
+        saveas(fig,fname_pdf);
+        close(fig);
+    end
+    
+    newstr = sprintf( '%s: Tracking data loaded\n', file );
+    refreshdisp( newstr, str );
+end

070c1454875556468ba8f7c4952646c04f48f70c
Manfredi Castelli <51319235+manfrediCastelli@users.noreply.github.com>
2019-07-17 11:44:36 +0100

    Update neuroSEE_segment.m

--- ROI_segmentation/neuroSEE_segment.m
+++ ROI_segmentation/neuroSEE_segment.m
@@ -22,15 +22,11 @@
 %   corr_image  : correlation image from green channel
 %   params      : parameters for specific roi segmentation method
 
-<<<<<<< HEAD
-function [cell_tsG, cell_tsR, masks, mean_imratio, params] = neuroSEE_segment(stack_g, stack_r, mean_r, data_locn, file, params, force)
-    
-    if nargin<7, force = 0;      end
-=======
+
 function [tsG, df_f, masks, corr_image, params] = neuroSEE_segment(imG, mean_imR, data_locn, file, mcorr_method, segment_method, params, force)
 
 if nargin<6, force = 0;      end
->>>>>>> e1ab0d31330487c7174b86c220baa2340bb264df
+
 
 filedir = [data_locn,'Data/',file(1:8),'/Processed/',file,'/mcorr_',mcorr_method,'/',segment_method,'/'];
 if ~exist( filedir, 'dir' ), mkdir( filedir ); end
@@ -83,21 +79,7 @@ if force
     saveas(fig,fname_fig,'pdf');
     close(fig);
     
-<<<<<<< HEAD
-    % If asked to force overwrite, run ABLE right away
-    if force
-        cellrad = params.cellrad;
-        maxcells = params.maxcells;
-        cell_tsR = zeros(1,size(stack_r,3));
-        [cell_tsG, masks, mean_imratio] = ABLE( stack_g, mean_r, file, cellrad, maxcells );
-        
-        % [cell_tsG, masks] = refineSegmentation(cell_tsG, masks, params);
-        for i = 1:size(masks,3)
-            ind = find(masks(:,:,i));
-            for j = 1:size(stack_r,3)
-                imR_reshaped = reshape(stack_r(:,:,j),size(stack_r,1)*size(stack_r,2),1);
-                cell_tsR(i,j) = mean(imR_reshaped(ind));
-=======
+
     fprintf('%s: ROI segmentation done\n',file);
 
 else
@@ -107,7 +89,7 @@ else
     % If timeseries mat file doesn't exist, do roi segmentation
     if ~yn_mat
         if strcmpi(segment_method,'ABLE')
-            [tsG, masks, ~, corr_image] = ABLE( imG, mean_imR, file, params.maxcells, params.cellrad );
+            [tsG, masks, ~, corr_image] = ABLE_manfredi( imG, mean_imR, file, params.maxcells, params.cellrad );
 
             % Calculate df_f
             df_f = zeros(size(tsG));
@@ -116,7 +98,7 @@ else
                 fo = ones(size(x)) * prctile(x,5);
                 df_f(i,:) = (tsG(i,:) - fo) ./ fo;
                 df_f(i,:) = lowpass( medfilt1(df_f(i,:),23), 1, params.imrate );
->>>>>>> e1ab0d31330487c7174b86c220baa2340bb264df
+
             end
 
         else
@@ -153,41 +135,13 @@ else
 
         fprintf('%s: ROI segmentation done\n',file);
         
-<<<<<<< HEAD
-        % If timeseries mat file doesn't exist, run ABLE
-        if ~yn_ts
-            cellrad = params.cellrad;
-            maxcells = params.maxcells;
-            cell_tsR = zeros(1,size(stack_r,3));
-             [cell_tsG, masks, mean_imratio] = ABLE( stack_g, mean_r, file, cellrad, maxcells ); %
-%             [cell_tsG, masks, mean_imratio] = ABLE_manfredi( stack_g, stack_r, file, cellrad, maxcells );  % edit by Manfredi 
-            % [cell_tsG, masks] = refineSegmentation(cell_tsG, masks, params);
-            for i = 1:size(masks,3)
-                ind = find(masks(:,:,i));
-                for j = 1:size(stack_r,3)
-                    imR_reshaped = reshape(stack_r(:,:,j),size(stack_r,1)*size(stack_r,2),1);
-                    cell_tsR(i,j) = mean(imR_reshaped(ind));
-                end
-            end
-            
-            % Save output
-            % Plot masks on summary image and save plot
-            plotopts.plot_ids = 1; % set to 1 to view the ID number of the ROIs on the plot
-            fig = plotContoursOnSummaryImage(mean_imratio, masks, plotopts);
-            fname_fig = [filedir file '_2P_ROIs'];
-            savefig(fig,fname_fig);
-            saveas(fig,fname_fig,'pdf');
-            close(fig);
 
-            % Save masks in a mat file
-            save(fname_out,'cell_tsG','cell_tsR','masks','mean_imratio','params')
-=======
     else
         % If it exists, load it 
         segmentOutput = load(fname_mat);
         if isfield(segmentOutput,'cell_tsG')
             tsG = segmentOutput.cell_tsG;
->>>>>>> e1ab0d31330487c7174b86c220baa2340bb264df
+
         else
             tsG = segmentOutput.tsG;
         end
@@ -217,7 +171,4 @@ else
         cprintf(str)
     end
 end
-<<<<<<< HEAD
-=======
 
->>>>>>> e1ab0d31330487c7174b86c220baa2340bb264df

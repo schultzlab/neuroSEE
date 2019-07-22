cae99a1b8b34852f26c27c3265985394d0c6d129
Castelli <you@example.com>
2019-07-22 11:41:27 +0100

    Updates by Manfredi

--- pipelines/run_pipelineManfredi.m
+++ pipelines/run_pipelineManfredi.m
@@ -1,9 +1,9 @@
 % Written by Ann Go
-% 
+%
 % This script runs the complete data processing pipeline for a single
 % image file. Processing steps include:
 % (1) motion correction (and dezippering)
-% (2) roi segmentation 
+% (2) roi segmentation
 % (3) neuropil decontamination and timeseries extraction
 % (4) spike extraction
 % (5) tracking data extraction
@@ -17,7 +17,7 @@ clear; % close all;
 tic
 
 %% USER: Set basic methods
-                            
+
 test = false;                    % flag to use one of smaller files in test folder)
 default = true;                 % flag to use default parameters
                                 % flag to force
@@ -29,7 +29,7 @@ force = [false;...              % (1) motion correction even if motion corrected
          false];                % (6) force place field mapping
 
 mcorr_method = 'normcorre';     % [normcorre,fftRigid] CaImAn NoRMCorre method, fft-rigid method (Katie's)
-segment_method = 'ABLE';      % [ABLE,CaImAn] 
+segment_method = 'ABLE';      % [ABLE,CaImAn]
 dofissa = true;                 % flag to implement FISSA (when false, overrides force(3) setting)
 manually_refine_spikes = false; % flag to manually refine spike estimates
 manuall_refine_PFmap = false;   % flag to tweak PFmap including changing number of epochs or velocity threshold
@@ -40,7 +40,7 @@ manuall_refine_PFmap = false;   % flag to tweak PFmap including changing number 
 [data_locn,comp,err] = load_neuroSEEmodules(test);
 if ~isempty(err)
 %     beep
-%     cprintf('Errors',err);    
+%     cprintf('Errors',err);
     %return mANFREDI
 end
 if ~strcmpi(comp,'mac')
@@ -52,7 +52,7 @@ end
 
 %% USER: Specify file
 
-file = '20181016_09_44_06'; 
+file = '20181016_09_44_06';
 
 
 %% USER: Set parameters (if not using default)
@@ -70,8 +70,8 @@ if ~default
         % NoRMCorre
         if strcmpi(mcorr_method,'normcorre')
             params.nonrigid = NoRMCorreSetParms(...
-                'd1',512,...        % width of image [default: 512]  *Regardless of user-inputted value, neuroSEE_motioncorrect reads this 
-                'd2',512,...        % length of image [default: 512] *value from actual image    
+                'd1',512,...        % width of image [default: 512]  *Regardless of user-inputted value, neuroSEE_motioncorrect reads this
+                'd2',512,...        % length of image [default: 512] *value from actual image
                 'grid_size',[32,32],...     % default: [32,32]
                 'overlap_pre',[32,32],...   % default: [32,32]
                 'overlap_post',[32,32],...  % default: [32,32]
@@ -84,7 +84,7 @@ if ~default
                 'us_fac',50,...             % default: 50
                 'init_batch',200);          % default: 200
         end
-    % ROI segmentation 
+    % ROI segmentation
         params.ROIsegment.cellrad = 10;            % expected radius of a cell (pixels)    [default: 10]
         params.ROIsegment.maxcells = 200;          % estimated number of cells in FOV      [default: 200]
         params.ROIsegment.df_prctile = 5;          % percentile to be used for estimating baseline [default: 5]
@@ -107,7 +107,7 @@ if ~default
 end
 
 if test
-    params.ROIsegment.maxcells = 60;  
+    params.ROIsegment.maxcells = 60;
 end
 
 
@@ -115,8 +115,8 @@ end
 
 if default
     params = load( 'default_params.mat' );
-    
-    % Remove irrelevant parameters 
+
+    % Remove irrelevant parameters
     if strcmpi(mcorr_method,'normcorre')
         params = rmfield(params,'fftRigid');
 %         fields = {'df_prctile','df_medfilt1'};
@@ -124,7 +124,7 @@ if default
     else
         params = rmfield(params,'nonrigid');
     end
-    
+
     if ~dofissa
         params = rmfield(params,'fissa');
     end
@@ -137,17 +137,17 @@ params.methods.dofissa = dofissa;
 if str2double(file(1:4)) > 2018
     params.FOV = 490; % FOV area = FOV x FOV, FOV in um
 else
-    params.FOV = 330; 
+    params.FOV = 330;
 end
 
 
-%% Check if file has been processed. If not, continue processing unless forced to overwrite 
+%% Check if file has been processed. If not, continue processing unless forced to overwrite
 % existing processed data
 % check(1:6) check for existing data in processing steps 1-6
 % check(7) checks for existing mat file pooling all processed data for file
 
 
-data_locn = '/home/mc6017/Data/'; % data_locn on linux_box
+%data_locn = '/home/mc6017/Data/'; % data_locn on linux_box
 check = checkforExistingProcData(data_locn, file, mcorr_method, segment_method, dofissa);
 if check(7) % All processing steps have been done
     if ~any(force)
@@ -160,16 +160,16 @@ end
 if force(1)
     force([2:4,6]) = true; % because redoing motion correction step affects all succeeding steps except step 5
 elseif force(2)
-    force([3:4,6]) = true; 
+    force([3:4,6]) = true;
 elseif force(3)
-    force([4,6]) = true; 
+    force([4,6]) = true;
 elseif force(4)>0
     force(6) = true;
 end
 
 
 %% Image files
-% Load original image files if forced to do motion correction or if motion corrected files don't exist 
+% Load original image files if forced to do motion correction or if motion corrected files don't exist
 
 if force(1) || ~check(1)
     [imG,imR] = load_imagefile( data_locn, file );
@@ -177,12 +177,12 @@ else
     imG = []; imR = [];
 end
 
-            
+
 %% (1) Motion correction
-% Saved in file folder: 
-%   motion corrected tif files 
-%   summary fig & pdf, 
-%   mat with fields 
+% Saved in file folder:
+%   motion corrected tif files
+%   summary fig & pdf,
+%   mat with fields
 %       green.[ meanframe, meanregframe ]
 %       red.[ meanframe, meanregframe ]
 %       template
@@ -190,16 +190,16 @@ end
 %       col_shift
 %       params
 
-if any([ any(force(1:2)), ~check(2), ~check(1) ]) 
+if any([ any(force(1:2)), ~check(2), ~check(1) ])
     [imG, imR, ~, params] = neuroSEE_motionCorrect( imG, imR, data_locn, file, params, force(1) );
-else 
+else
     fprintf('%s: Motion corrected files found. Skipping motion correction\n', file);
     imG = []; imR = [];
 end
 
 
 %% (2) ROI segmentation
-% Saved in file folder: 
+% Saved in file folder:
 %   correlation image with ROIs (fig, pdf)
 %   mat with fields {tsG, df_f, masks, corr_image, params}
 
@@ -236,34 +236,34 @@ end
 
 if manually_refine_spikes
     GUI_manually_refine_spikes( spikes, tsG, dtsG, df_f, ddf_f, data_locn, file, params, corr_image, masks );
-    uiwait 
+    uiwait
 end
 
 
 % %% (5) Find tracking file then load it
 % % Saved: fig & pdf of trajectory
 % %        mat with fields {time, r, phi, x, y , speed, w, alpha, TTLout, filename}
-% 
+%
 % fname_track = findMatchingTrackingFile( data_locn, file, force(5) );
 % trackData = load_trackfile( data_locn,file, fname_track, force(5) );
 % if any(trackData.r < 100)
 %     params.mode_dim = '2D'; % open field
-%     params.PFmap.Nbins = [10, 10]; % number of location bins in [x y]               
-% else 
+%     params.PFmap.Nbins = [10, 10]; % number of location bins in [x y]
+% else
 %     params.mode_dim = '1D'; % circular linear track
-%     params.PFmap.Nbins = 30;      % number of location bins               
+%     params.PFmap.Nbins = 30;      % number of location bins
 % end
-% 
-% 
+%
+%
 % %% (6) Generate place field maps
 % % Saved: fig & pdf of summary consisting of occMap, infoMap, spikeMap and placeMap
 % %        mat file with fields {occMap, spikeMap, infoMap, placeMap, downData, activeData,...
 % %                               placeMap_smooth, sorted_placeMap, sortIdx, params}
-% 
+%
 % if manually_refine_spikes
 %     force(6) = true;
 % end
-% 
+%
 % % if strcmpi(params.mode_dim,'2D')
 % %     [occMap, spikeMap, infoMap, placeMap, placeMap_smooth, placeMap_asd, ...
 % %      downData, activeData, params] ...
@@ -276,10 +276,10 @@ end
 % %      placeMap_pertrial, normplaceMap_pertrial, PCidx, sortIdx]...
 % %       = neuroSEE_mapPF( spikes, trackData, data_locn, file, params, force(6));
 % % end
-% % 
-% 
+% %
+%
 % %% Save output. These are all the variables needed for viewing data with GUI
-% 
+%
 % % save(fname_allData,'file','corr_image','masks','tsG','dtsG','df_f','ddf_f','spikes',...
 % %                     'fname_track','downData','activeData','occMap','spikeMap','infoMap',...
 % %                     'placeMap','placeMap_smooth','params');

c3299e505d99e013427439b9900ceae613e3ed79
Ann Go <m.go@imperial.ac.uk>
2020-09-08 11:46:02 +0100

    test run

--- pipelines/run_imreg_global.m
+++ pipelines/run_imreg_global.m
@@ -2,4 +2,12 @@ file = '20181016_09_09_43';
 templateglob = '20181015_09_37_54';
 imregr_params = load('CtoB_r.mat');
 imregnr_params = load('CtoB_nr.mat');
+
+[data_locn,~,err] = load_neuroSEEmodules;
+if ~isempty(err)
+    beep
+    cprintf('Errors',err);    
+    return
+end
+
 imreg_global( file, templateglob, imregr_params, imregnr_params )

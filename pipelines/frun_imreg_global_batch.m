80b7d3735ef1b2f2518807330b4b5cc2ea05d8cd
Ann Go <m.go@imperial.ac.uk>
2020-09-09 08:13:17 +0100

    added load_neurosSEEmodules statement

--- pipelines/frun_imreg_global_batch.m
+++ pipelines/frun_imreg_global_batch.m
@@ -9,4 +9,11 @@ imregnr_params = load([templateloccode 'toB_nr.mat']);
     A = struct('A', repmat(shifts, 7420, 1));
     imregnr_params.shifts = A.A;
 
+[~,~,err] = load_neuroSEEmodules;
+if ~isempty(err)
+    beep
+    cprintf('Errors',err);    
+    return
+end
+
 imreg_global( array_id, list, templateglob, imregr_params, imregnr_params, templateloc, false )

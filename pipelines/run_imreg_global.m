9a945d8734ec2d91e07c8ee178e84b6afa01efcf
Ann Go <m.go@imperial.ac.uk>
2020-09-09 04:55:13 +0100

    removed redundant statments

--- pipelines/run_imreg_global.m
+++ pipelines/run_imreg_global.m
@@ -1,13 +1,7 @@
 file = '20181016_09_09_43';
 templateglob = '20181015_09_37_54';
 imregr_params = load('CtoB_r.mat');
-shifts = imregr_params.shifts;
-    A = struct('A', repmat(shifts, 7420, 1));
-    imregr_params.shifts = A.A;
 imregnr_params = load('CtoB_nr.mat');
-    shifts = imregnr_params.shifts;
-    A = struct('A', repmat(shifts, 7420, 1));
-    imregnr_params.shifts = A.A;
     
 [data_locn,~,err] = load_neuroSEEmodules;
 if ~isempty(err)

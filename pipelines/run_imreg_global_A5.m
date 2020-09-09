5c8b4ece726465b91829ca27b6fb628e2b9c30e5
Ann Go <m.go@imperial.ac.uk>
2020-09-09 17:04:15 +0100

    test run

---
+++ pipelines/run_imreg_global_A5.m
@@ -0,0 +1,20 @@
+file = '20181013_10_59_42';
+templateglob = '20181015_09_37_54';
+templateloc = '20181013_10_53_51';
+imregr_params = load('AtoB_r.mat');
+shifts = imregr_params.shifts;
+    A = struct('A', repmat(shifts, 7420, 1));
+    imregr_params.shifts = A.A;
+imregnr_params = load('AtoB_nr.mat');
+    shifts = imregnr_params.shifts;
+    A = struct('A', repmat(shifts, 7420, 1));
+    imregnr_params.shifts = A.A;
+    
+[data_locn,~,err] = load_neuroSEEmodules;
+if ~isempty(err)
+    beep
+    cprintf('Errors',err);    
+    return
+end
+
+imreg_global( file, templateglob, imregr_params, imregnr_params, templateloc );

69771c6ecd1a93cdffe3bb5ee55c2ba96fa4a382
Ann Go <m.go@imperial.ac.uk>
2020-09-09 10:52:25 +0100

    test run

--- pipelines/run_imreg_global_A2.m
+++ pipelines/run_imreg_global_A2.m
@@ -1,10 +1,10 @@
-file = '20181013_10_33_18';
+file = '20181013_10_43_47';
 templateglob = '20181013_10_53_51';
-imregr_params = load('A1toA_r.mat');
+imregr_params = load('A2toA_r.mat');
 shifts = imregr_params.shifts;
     A = struct('A', repmat(shifts, 7420, 1));
     imregr_params.shifts = A.A;
-imregnr_params = load('A1toA_nr.mat');
+imregnr_params = load('A2toA_nr.mat');
     shifts = imregnr_params.shifts;
     A = struct('A', repmat(shifts, 7420, 1));
     imregnr_params.shifts = A.A;

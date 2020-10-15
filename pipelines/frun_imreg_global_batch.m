ebe33e8f6d25bad2e9993032ca1507b03c5e410e
Ann Go <m.go@imperial.ac.uk>
2020-10-15 12:59:39 +0100

    data proc run

--- pipelines/frun_imreg_global_batch.m
+++ pipelines/frun_imreg_global_batch.m
@@ -4,12 +4,12 @@ if nargin<6, force = []; end
 imregr_params = load([shiftsfile_prefix '_r.mat']);
     shifts = imregr_params.params{array_id}.shifts;
     A = struct('A', repmat(shifts, 7420, 1));
-    imregr_params.shifts = A.A;
+    imregr_params.params{array_id}.shifts = A.A;
 
 imregnr_params = load([shiftsfile_prefix '_nr.mat']);
     shifts = imregnr_params.params{array_id}.shifts;
     A = struct('A', repmat(shifts, 7420, 1));
-    imregnr_params.shifts = A.A;
+    imregnr_params.params{array_id}.shifts = A.A;
 
 [~,~,err] = load_neuroSEEmodules;
 if ~isempty(err)
@@ -18,4 +18,4 @@ if ~isempty(err)
     return
 end
 
-imreg_global_batch( array_id, list, templateglob, imregr_params, imregnr_params, templateloc, force );
+imreg_global_batch( array_id, list, templateglob, imregr_params.params{array_id}, imregnr_params.params{array_id}, templateloc, force );

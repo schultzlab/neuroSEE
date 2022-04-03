bc0697d0eeeaa07e40d1a4f30330781dc7249a81
Ann Go <m.go@imperial.ac.uk>
2022-04-03 20:36:20 +0100

    trial run

---
+++ pipelines/frun_imreg_global.m
@@ -0,0 +1,21 @@
+function frun_imreg_global(array_id, list, templateglob, templateloc, shiftsfile_prefix, force)
+if nargin<6, force = []; end
+
+imregr_params = load([shiftsfile_prefix '_r.mat']);
+    shifts = imregr_params.params.shifts;
+    A = struct('A', repmat(shifts, 7420, 1));
+    imregr_params.params.shifts = A.A;
+
+imregnr_params = load([shiftsfile_prefix '_nr.mat']);
+    shifts = imregnr_params.params.shifts;
+    A = struct('A', repmat(shifts, 7420, 1));
+    imregnr_params.params.shifts = A.A;
+
+[~,~,err] = load_neuroSEEmodules;
+if ~isempty(err)
+    beep
+    cprintf('Errors',err);    
+    return
+end
+
+imreg_global_batch( array_id, list, templateglob, imregr_params.params{array_id}, imregnr_params.params{array_id}, templateloc, force );

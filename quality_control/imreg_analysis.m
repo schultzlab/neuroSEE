b422713a9a9a9b35c9ccb6490abeae6bff4ae1ff
Ann Go <m.go@imperial.ac.uk>
2022-04-19 15:35:55 +0100

    trial run

--- quality_control/imreg_analysis.m
+++ quality_control/imreg_analysis.m
@@ -6,7 +6,7 @@ if ~isempty(err)
     return
 end
 
-list = 'list_m66_fam1nov.txt'; % SPECIFY LIST
+list = 'list_m70_fam1fam2.txt'; % SPECIFY LIST
 listfile = [data_locn 'Digital_Logbook/lists_imaging/' list];
 files = extractFilenamesFromTxtfile( listfile );
 
@@ -24,15 +24,15 @@ params = neuroSEE_setparams(...
             'max_shift_nr', 30,...
             'grid_size_nr', [g,g],...
             'iter',1,...
-            'max_dev',29,... % I normally tweak this. I found that 7-9 is usually good enough.   
+            'max_dev',9,... % I normally tweak this. I found that 7-9 is usually good enough.   
             'overlap_pre', [g/4,g/4],... 
             'min_patch_size', [g/4,g/4],...      
             'min_diff', [g/8,g/8]);         
         
 %%           
-n = 8; % SPECIFY FILE # to register
+n = 13; % SPECIFY FILE # to register
 regchannel = 1; % 1:green, 2:red
-refind = 6; % SPECIFY FILE # for reference 
+refind = 1; % SPECIFY FILE # for reference 
 %%
 
 if regchannel == 1
@@ -49,6 +49,8 @@ end
 
 [YY, shifts_r, ~, options_r, col_shift_r] = normcorre(Y,params.mcorr.normcorre_r,ref_ch1);
 [YY, shifts_nr, ~,options_nr,col_shift_nr] = normcorre(YY,params.mcorr.normcorre_nr,ref_ch1);
+% [ YY, XX, ~, ~, col_shift_r, shifts_r, ~, ~ ] = normcorre_2ch( Y, X, params.mcorr.normcorre_r, ref_ch1 );
+% [ YY, XX, ~, ~, col_shift_nr, shifts_nr, ~, ~ ] = normcorre_2ch( YY, XX, params.mcorr.normcorre_nr, ref_ch1 );
 figure;
 fy = imfuse( YY, ref_ch1, 'falsecolor', 'Scaling', 'joint', 'ColorChannels', [1 2 0]);
 imshow(fy); title(titlestr1);

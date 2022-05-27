0659fcfd5e33b7eb65a801e1100a113d7861c815
Ann Go <m.go@imperial.ac.uk>
2022-05-27 18:43:41 +0100

    edit by Ann

--- quality_control/frun_redoFISSA.m
+++ quality_control/frun_redoFISSA.m
@@ -7,30 +7,8 @@ if ~isempty(err)
     return
 end
 
-%% Find mouse ID and environment names
-[mouseid,expname,fov] = find_mouseIDexpname( list );
-env{1} = 'fam1';
-ind1 = strfind(expname,'fam1');
-if numel(ind1) > 2
-    env{3} = 'fam1r2';
-    if numel(strfind(expname,'fam1rev')) > 0
-        env{2} = 'fam1rev';
-    end
-elseif numel(ind1) == 2
-    if numel(strfind(expname,'fam1rev')) > 0
-        env{2} = 'fam1rev';
-    else
-        env{3} = 'fam1r2';
-    end
-end
-if numel(strfind(expname,'fam2')) > 0
-    env{2} = 'fam2';
-end
-if numel(strfind(expname,'nov')) > 0
-    env{2} = 'nov';
-end
-
 %% Compare number of ROIs in segment_output and FISSA_output
+[mouseid,expname,fov] = find_mouseIDexpname( list );
 dir_concenv = [data_locn 'Analysis/' mouseid '/' fov '/' mouseid '_' expname ...
                 '/group_proc/imreg_normcorre_CaImAn/' mouseid '_' expname '_imreg_ref' reffile '/'];
 if ~exist(dir_concenv,'dir')            
@@ -45,9 +23,11 @@ if exist([dir_concenv '/FISSA/' mouseid '_' expname '_ref' reffile '_fissa_outpu
     num_rois_fissa = size(fissa.dtsG,1);
 
     if num_rois_so ~= num_rois_fissa 
+        fprintf('%s: ROI numbers not the same for segment_output and fissa_output. Redoing FISSA.\n', [mouseid '_' expname '_ref' reffile]);
         frun_pipeline_imreg( list, reffile, true, [0; 0; 1; 0; 0; 0], [1; 1; 1; 1; 1; 1] )
     end
 else
+    fprintf('%s: FISSA_output does not exist. Now doing FISSA.\n', [mouseid '_' expname '_ref' reffile]);
     frun_pipeline_imreg( list, reffile, true, [0; 0; 1; 0; 0; 0], [1; 1; 1; 1; 1; 1] )
 end
 end

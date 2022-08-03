2914c90b8772efd9e584217ce97c7e40cc719f5a
Ann Go <m.go@imperial.ac.uk>
2022-08-03 17:32:57 +0100

    edited to output calc_deltaf outputs

--- neuropil_decontamination/runFISSA.py
+++ neuropil_decontamination/runFISSA.py
@@ -20,6 +20,8 @@ Python script to run Fissa through Python. Script takes three inputs from the co
 import os
 import sys
 import fissa
+import scipy.io
+
 # from runFISSA import runFISSA
    
 # tifdir   = '/Volumes/thefarm2/live/CrazyEights/AD_2PCa/Data/20181016/Processed/20181016_09_49_06/FISSAtest/'
@@ -38,6 +40,10 @@ experiment = fissa.Experiment(tiffile, roizip, outdir)
 experiment.separate(redo_sep=True, redo_prep=True)
 # calculate df/f0
 experiment.calc_deltaf(freq=30.9)
+file_path_raw = outdir + 'deltaf_raw.mat'
+file_path_result = outdir + 'deltaf_result.mat'
+scipy.io.savemat(file_path_raw, {'deltaf_raw': experiment.deltaf_raw})
+scipy.io.savemat(file_path_result, {'deltaf_result': experiment.deltaf_result})
 # save to matlab
 experiment.save_to_matlab()
 

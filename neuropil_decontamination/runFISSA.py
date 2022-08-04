1591455441d1686c687091153ac20c700cfdc393
Ann Go <m.go@imperial.ac.uk>
2022-08-04 18:00:04 +0100

    now allows multiple tiff filenames to be specified as input

--- neuropil_decontamination/runFISSA.py
+++ neuropil_decontamination/runFISSA.py
@@ -3,7 +3,7 @@
 """
 Created on Fri Jun  7 17:13:55 2019
 
-@author: cedavey, sihaolu
+@author: cedavey, sihaolu, anngo
 
 Python script to run Fissa through Python. Script takes three inputs from the command line in order:
  - tifdir : directory containing tif stack
@@ -20,7 +20,6 @@ Python script to run Fissa through Python. Script takes three inputs from the co
 import os
 import sys
 import fissa
-import scipy.io
 
 # from runFISSA import runFISSA
    
@@ -28,22 +27,17 @@ import scipy.io
 # roizip   = '/Volumes/thefarm2/live/CrazyEights/AD_2PCa/Data/20181016/Processed/20181016_09_49_06/FISSAtest/rois.zip'
 # outdir   = '/Volumes/thefarm2/live/CrazyEights/AD_2PCa/Data/20181016/Processed/20181016_09_49_06/FISSAtest/out'
 
-tiffile = sys.argv[1]
+tiffile = [sys.argv[1]]
 roizip = sys.argv[2]
 outdir = sys.argv[3]
 
-
-
 # generate an experiment object
+# (Ann: I edited fissa.core to include deltaf_raw and deltaf_result in the matlab outputs as they weren't being saved)
 experiment = fissa.Experiment(tiffile, roizip, outdir)
 # separate neuropil
 experiment.separate(redo_sep=True, redo_prep=True)
 # calculate df/f0
 experiment.calc_deltaf(freq=30.9)
-file_path_raw = outdir + 'deltaf_raw.mat'
-file_path_result = outdir + 'deltaf_result.mat'
-scipy.io.savemat(file_path_raw, {'deltaf_raw': experiment.deltaf_raw})
-scipy.io.savemat(file_path_result, {'deltaf_result': experiment.deltaf_result})
 # save to matlab
 experiment.save_to_matlab()
 

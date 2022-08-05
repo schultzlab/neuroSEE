2d04f8c632a4055c5fa0ff7eaa926c9f4790fe63
Ann Go <m.go@imperial.ac.uk>
2022-08-05 06:41:16 +0100

    FISSA now takes as input list of files

--- neuropil_decontamination/runFISSA.py
+++ neuropil_decontamination/runFISSA.py
@@ -2,17 +2,19 @@
 # -*- coding: utf-8 -*-
 """
 Created on Fri Jun  7 17:13:55 2019
+Edited on Thu Aug   5          2022
 
 @author: cedavey, sihaolu, anngo
 
-Python script to run Fissa through Python. Script takes three inputs from the command line in order:
- - tifdir : directory containing tif stack
- - roizip : zip file of ROIs generated through `generateImageJROIfiles.m
+Python script to run Fissa through Python command line. Script takes three inputs from the command line in order:
+ - tiff : can be directory containing tif stacks/s, can be tif filename, or list of tif filenames
+ - roizip : zip file of ROIs generated through 'generateImageJROIfiles.m'
  - outdir : directory to save the Fissa output into
 
  Example:
  ```
  $ python runFISSA.py FISSAtest/ FISSAtest/rois.zip FISSAtest/out/
+ $ python runFISSA.py folder1/tiffile1.tif,folder2/tiffile2.tif,folder3/tiffile3 FISSAtest/rois.zip FISSAtest/out/
  ```
 
 """
@@ -21,19 +23,14 @@ import os
 import sys
 import fissa
 
-# from runFISSA import runFISSA
-   
-# tifdir   = '/Volumes/thefarm2/live/CrazyEights/AD_2PCa/Data/20181016/Processed/20181016_09_49_06/FISSAtest/'
-# roizip   = '/Volumes/thefarm2/live/CrazyEights/AD_2PCa/Data/20181016/Processed/20181016_09_49_06/FISSAtest/rois.zip'
-# outdir   = '/Volumes/thefarm2/live/CrazyEights/AD_2PCa/Data/20181016/Processed/20181016_09_49_06/FISSAtest/out'
-
-tiffile = [sys.argv[1]]
 roizip = sys.argv[2]
 outdir = sys.argv[3]
 
+tiff = list(sys.argv[1].split(","))
+
 # generate an experiment object
 # (Ann: I edited fissa.core to include deltaf_raw and deltaf_result in the matlab outputs as they weren't being saved)
-experiment = fissa.Experiment(tiffile, roizip, outdir)
+experiment = fissa.Experiment(tiff, roizip, outdir)
 # separate neuropil
 experiment.separate(redo_sep=True, redo_prep=True)
 # calculate df/f0
---
+++ pipelines/untitled.m
@@ -0,0 +1,23 @@
+[data_locn,comp,err] = load_neuroSEEmodules;
+list = 'list_m62_fov2_fam1fam2-fam1.txt';
+listfile = [data_locn 'Digital_Logbook/lists_imaging/' list];
+files = extractFilenamesFromTxtfile( listfile );
+
+tifflist = [];
+for n = 1:size(files,1)
+    file = files(n,:);
+    tiffile = [data_locn 'Data/' file(1:8) '/Processed/' file '/mcorr_normcorre/' file '_2P_XYT_green_mcorr.tif'];
+    if n>1
+        tifflist = [tifflist ',' tiffile];
+    else
+        tifflist = tiffile;
+    end
+end
+
+load('/Volumes/thefarm2/live/CrazyEights/AD_2PCa/Analysis/m62/fov2/m62_fov2_fam1fam2-fam1/group_proc/imreg_normcorre_CaImAn/m62_fov2_fam1fam2-fam1_imreg_ref20181013_10_53_51_concenvrois/m62_fov2_fam1fam2-fam1_ref20181013_10_53_51_segment_output.mat')
+fissadir = '/Volumes/thefarm2/live/CrazyEights/AD_2PCa/Analysis/m62/fov2/m62_fov2_fam1fam2-fam1/group_proc/imreg_normcorre_CaImAn/m62_fov2_fam1fam2-fam1_imreg_ref20181013_10_53_51_concenvrois/FISSA/';
+
+runFISSA( masks, tifflist, fissadir )
+% dtsG = tsG; ddf_f = df_f; spikes = tsG;
+% GUI_viewtimeseries(tsG, df_f, dtsG, ddf_f, spikes)
+

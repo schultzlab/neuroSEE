fb712d0edafe55c7e84adf5fed4afadab054286d
Ann Go <m.go@imperial.ac.uk>
2022-08-05 11:51:05 +0100

    trial run

--- pipelines/untitled.m
+++ pipelines/untitled.m
@@ -17,7 +17,7 @@ end
 load([data_locn 'Analysis/m62/fov2/m62_fov2_fam1fam2-fam1/group_proc/imreg_normcorre_CaImAn/m62_fov2_fam1fam2-fam1_imreg_ref20181013_10_53_51_concenvrois/m62_fov2_fam1fam2-fam1_ref20181013_10_53_51_segment_output.mat'])
 fissadir = [data_locn 'Analysis/m62/fov2/m62_fov2_fam1fam2-fam1/group_proc/imreg_normcorre_CaImAn/m62_fov2_fam1fam2-fam1_imreg_ref20181013_10_53_51_concenvrois/FISSA/'];
 
-% runFISSA( masks, tifflist, fissadir )
+runFISSA( masks, tifflist, fissadir )
 
 result = load([fissadir 'FISSAout/matlab.mat'],'result');
 deltaf_result = load([fissadir 'FISSAout/matlab.mat'],'deltaf_result');

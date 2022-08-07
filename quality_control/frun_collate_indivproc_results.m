ddab8e2d77417e874ecbee1220df3b81c47812a5
Ann Go <m.go@imperial.ac.uk>
2022-08-07 15:38:04 +0100

    excluded PFmaps

--- quality_control/frun_collate_indivproc_results.m
+++ quality_control/frun_collate_indivproc_results.m
@@ -2,7 +2,7 @@
 %
 % This script collates the results for the complete pipeline processing of
 % the individual files in 'list'. Figures comparing average motion
-% corrected frames, PF maps, ROIs and trajectories are generated and saved
+% corrected frames, ROIs and trajectories are generated and saved
 % in
 %   /...thefarm2/CrazyEights/AD_2PCa/Analysis/m##/m##_expname/individual_proc/
 %
@@ -14,13 +14,12 @@
 %           'list_m79_fam1_s1-5.txt'            - all fam1 files across 5 sessions           
 %           'list_m86_open_s1-2.txt'            - all open field files across 2 sessions
 %
-% force  : (optional) flag to force generation of comparison figures even though they
-%           already exist (default: false)
+% force  : (optional, default: false) flag to force generation of comparison figures even though they
+%           already exist
 
-function frun_collate_indivproc_results( list, mcorr_method, force )
+function frun_collate_indivproc_results( list, force )
 
-if nargin<3, force = false; end
-if nargin<2, mcorr_method = 'normcorre'; end
+if nargin<2, force = false; end
 
 addpath(genpath('../pipelines'));
 
@@ -33,6 +32,7 @@ if ~isempty(err)
 end
 
 % mcorr_method = 'normcorre';
+mcorr_method = 'normcorre';
 segment_method = 'CaImAn';
 dofissa = true;
     if dofissa, str_fissa = 'FISSA'; else, str_fissa = 'noFISSA'; end
@@ -51,7 +51,7 @@ Nfig = (Nfiles/nPlot)-1;
 if Nfig < 0, Nfig = 0; end
 
 
-%% Load image data for each recording
+%% Load image data for each file/recording
 if ~isempty(fov)
     sdir1 = [data_locn 'Analysis/' mouseid '/' fov '/' mouseid '_' expname '/individual_proc/indiv_' ...
          mcorr_method '_' segment_method '_' str_fissa '/indiv_mcorr/'];
@@ -62,7 +62,6 @@ end
     
 if any([ force, ~exist([sdir1 mouseid '_' expname '_GREEN_mcorr.fig'],'file'),...
                 ~exist([sdir1 mouseid '_' expname '_RED_mcorr.fig'],'file') ])
-                
     for i = 1:Nfiles
         file = files(i,:);
         fname = [data_locn 'Data/' file(1:8) '/Processed/' file '/mcorr_' mcorr_method '/' file '_mcorr_output.mat'];
@@ -74,7 +73,7 @@ if any([ force, ~exist([sdir1 mouseid '_' expname '_GREEN_mcorr.fig'],'file'),..
     end
     clear c
 
-    %% Compare green and red images to see which ones are most similar
+    % Compare green and red images to see which ones are most similar
 
     if exist('M','var') && isfield(M,'green')
         for ii=0:Nfig
@@ -136,129 +135,7 @@ if any([ force, ~exist([sdir1 mouseid '_' expname '_GREEN_mcorr.fig'],'file'),..
 end
 
 
-%% Load tracking data for each recording
-if ~isempty(fov)
-    sdir2 = [data_locn 'Analysis/' mouseid '/' fov '/' mouseid '_' expname '/individual_proc/indiv_' ...
-         mcorr_method '_' segment_method '_' str_fissa '/indiv_trajectories/'];
-else
-    sdir2 = [data_locn 'Analysis/' mouseid '/' mouseid '_' expname '/individual_proc/indiv_' ...
-         mcorr_method '_' segment_method '_' str_fissa '/indiv_trajectories/'];
-end
-    
-if force || ~exist([sdir2 mouseid '_' expname '_traj.fig'],'file')
-    for i = 1:Nfiles
-        trackfile = findMatchingTrackingFile(data_locn, files(i,:), 0);
-        c = load_trackfile(data_locn, files(i,:), trackfile, 0);
-        M(i).trackdata.x = c.x;
-        M(i).trackdata.y = c.y;
-        M(i).trackdata.r = c.r;
-        M(i).trackdata.phi = c.phi;
-        M(i).trackdata.speed = c.speed;
-        M(i).trackdata.time = c.time;
-    end
-    clear c
-
-    if exist('M','var') && isfield(M,'trackdata')
-        %% Compare trajectories
-        for ii=0:Nfig
-            fh = figure;
-            ha = tight_subplot(nRow,nCol,[.04 .01],[.01 .07],[.01 .01]);
-            for jj=0:nPlot-1
-                if (ii*nPlot+jj+1) <= Nfiles
-                    if ~isempty(M(ii*nPlot+jj+1).trackdata)
-                        axes(ha(ii*nPlot+jj+1));
-                        plot(M(ii*nPlot+jj+1).trackdata.x,M(ii*nPlot+jj+1).trackdata.y); 
-                        axis off; 
-                        str = files(ii*nPlot+jj+1,:);
-                        title([str(5:6) '-' str(7:8) ' ' str(10:11) ':' str(13:14)]);
-                    end
-                end
-            end
-            axes('Position',[0 0 1 1],'Xlim',[0 1],'Ylim',[0  1],'Box','off',...
-                'Visible','off','Units','normalized', 'clipping' , 'off');
-                titletext = [mouseid '-' expname ': trajectories'];
-                ind = strfind(titletext,'_');
-                titletext(ind) = '-';
-                text('Position',[0.01 0.99], 'FontSize',14, 'String',titletext);
-            if ~exist(sdir2,'dir'), mkdir(sdir2); fileattrib(sdir2,'+w','g','s'); end
-            fname_fig = [sdir2 mouseid '_' expname '_traj.fig'];
-            savefig( fh, fname_fig );
-            saveas( fh, fname_fig(1:end-4), 'png' );
-            close( fh );
-        end 
-    else
-        fprintf('%s: No tracking files found\n', list)
-    end
-else
-    trackfile = findMatchingTrackingFile(data_locn, files(1,:), 0);
-    c = load_trackfile(data_locn, files(1,:), trackfile, 0);
-    M(1).trackdata.r = c.r;
-end
-
-
-%% Load PF maps
-% Determine mode_dim
-if any(M(1).trackdata.r < 100)
-    mode_dim = '2D'; % open field
-else 
-    mode_dim = '1D'; % circular linear track
-end
-
-if ~isempty(fov)
-    sdir3 = [data_locn 'Analysis/' mouseid '/' fov '/' mouseid '_' expname '/individual_proc/indiv_' ...
-         mcorr_method '_' segment_method '_' str_fissa '/indiv_PFmaps/'];
-else 
-    sdir3 = [data_locn 'Analysis/' mouseid '/' mouseid '_' expname '/individual_proc/indiv_' ...
-         mcorr_method '_' segment_method '_' str_fissa '/indiv_PFmaps/'];
-end
-    
-if (force || ~exist([sdir3 mouseid '_' expname '_PFmaps.fig'],'file')) && strcmpi(mode_dim,'1D')
-    for i = 1:Nfiles
-        file = files(i,:);
-        PFmapfile = [data_locn 'Data/' file(1:8) '/Processed/' file '/mcorr_' mcorr_method '/' segment_method '/'...
-                     str_fissa '/PFmaps/' file '_PFmap_output.mat'];
-        if exist(PFmapfile,'file')
-            c = load(PFmapfile);
-            M(i).PFmap = c.hist.sort_normpfMap_sm(:,:,1);
-        end
-    end
-    clear c
-
-    if exist('M','var') && isfield(M,'PFmap')
-        %% Compare PF maps
-        for ii=0:Nfig
-            fh = figure;
-            ha = tight_subplot(nRow,nCol,[.04 .01],[.01 .07],[.01 .01]);
-            for jj=0:nPlot-1
-                if (ii*nPlot+jj+1) <= Nfiles
-                    if ~isempty(M(ii*nPlot+jj+1).PFmap)
-                        axes(ha(ii*nPlot+jj+1));
-                        imagesc(M(ii*nPlot+jj+1).PFmap); 
-                        axis off; 
-                        str = files(ii*nPlot+jj+1,:);
-                        title([str(5:6) '-' str(7:8) ' ' str(10:11) ':' str(13:14)]);
-                    end
-                end
-            end
-            axes('Position',[0 0 1 1],'Xlim',[0 1],'Ylim',[0  1],'Box','off',...
-                'Visible','off','Units','normalized', 'clipping' , 'off');
-                titletext = [mouseid '-' expname ': PF maps'];
-                ind = strfind(titletext,'_');
-                titletext(ind) = '-';
-                text('Position',[0.01 0.99], 'FontSize',14, 'String',titletext);
-            if ~exist(sdir3,'dir'), mkdir(sdir3); fileattrib(sdir3,'+w','g','s'); end
-            fname_fig = [sdir3 mouseid '_' expname '_PFmaps.fig'];
-            savefig( fh, fname_fig );
-            saveas( fh, fname_fig(1:end-4), 'png' );
-            close( fh );
-        end 
-    else
-        fprintf('%s: PF maps not found\n',list)
-    end
-end
-
-
-%% Load ROIs
+%% Load ROIs for each recording
 if ~isempty(fov)
     sdir4 = [data_locn 'Analysis/' mouseid '/' fov '/' mouseid '_' expname '/individual_proc/indiv_' ...
          mcorr_method '_' segment_method '_' str_fissa '/indiv_ROIs/'];
@@ -319,6 +196,67 @@ if (force || ~exist([sdir4 mouseid '_' expname '_ROIs.fig'],'file'))
         fprintf('%s: ROIs not found\n', list)
     end
 end
+
+%% Load tracking data for each recording
+if ~isempty(fov)
+    sdir2 = [data_locn 'Analysis/' mouseid '/' fov '/' mouseid '_' expname '/individual_proc/indiv_' ...
+         mcorr_method '_' segment_method '_' str_fissa '/indiv_trajectories/'];
+else
+    sdir2 = [data_locn 'Analysis/' mouseid '/' mouseid '_' expname '/individual_proc/indiv_' ...
+         mcorr_method '_' segment_method '_' str_fissa '/indiv_trajectories/'];
+end
+    
+if force || ~exist([sdir2 mouseid '_' expname '_traj.fig'],'file')
+    for i = 1:Nfiles
+        trackfile = findMatchingTrackingFile(data_locn, files(i,:), 0);
+        c = load_trackfile(data_locn, files(i,:), trackfile, 0);
+        M(i).trackdata.x = c.x;
+        M(i).trackdata.y = c.y;
+        M(i).trackdata.r = c.r;
+        M(i).trackdata.phi = c.phi;
+        M(i).trackdata.speed = c.speed;
+        M(i).trackdata.time = c.time;
+    end
+    clear c
+
+    if exist('M','var') && isfield(M,'trackdata')
+        % Compare trajectories
+        for ii=0:Nfig
+            fh = figure;
+            ha = tight_subplot(nRow,nCol,[.04 .01],[.01 .07],[.01 .01]);
+            for jj=0:nPlot-1
+                if (ii*nPlot+jj+1) <= Nfiles
+                    if ~isempty(M(ii*nPlot+jj+1).trackdata)
+                        axes(ha(ii*nPlot+jj+1));
+                        plot(M(ii*nPlot+jj+1).trackdata.x,M(ii*nPlot+jj+1).trackdata.y); 
+                        axis off; 
+                        str = files(ii*nPlot+jj+1,:);
+                        title([str(5:6) '-' str(7:8) ' ' str(10:11) ':' str(13:14)]);
+                    end
+                end
+            end
+            axes('Position',[0 0 1 1],'Xlim',[0 1],'Ylim',[0  1],'Box','off',...
+                'Visible','off','Units','normalized', 'clipping' , 'off');
+                titletext = [mouseid '-' expname ': trajectories'];
+                ind = strfind(titletext,'_');
+                titletext(ind) = '-';
+                text('Position',[0.01 0.99], 'FontSize',14, 'String',titletext);
+            if ~exist(sdir2,'dir'), mkdir(sdir2); fileattrib(sdir2,'+w','g','s'); end
+            fname_fig = [sdir2 mouseid '_' expname '_traj.fig'];
+            savefig( fh, fname_fig );
+            saveas( fh, fname_fig(1:end-4), 'png' );
+            close( fh );
+        end 
+    else
+        fprintf('%s: No tracking files found\n', list)
+    end
+else
+    trackfile = findMatchingTrackingFile(data_locn, files(1,:), 0);
+    c = load_trackfile(data_locn, files(1,:), trackfile, 0);
+    M(1).trackdata.r = c.r;
+end
+
+
 
 
 

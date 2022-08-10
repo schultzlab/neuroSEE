6af0930f8cba798e346196834a26c28e3863099c
Ann Go <m.go@imperial.ac.uk>
2022-08-10 18:07:21 +0100

    test run

--- pipelines/spiketest_caiman.m
+++ pipelines/spiketest_caiman.m
@@ -10,32 +10,33 @@ try
                 mouseid '_' expname '_imreg_ref' reffile '/'];
     segmentout = load([grp_sdir mouseid '_' expname '_ref' reffile '_segment_output.mat']);
 
+    bl_prctile = 83;
     params = neuroSEE_setparams(...
                 'FOV',490,...
                 'virus','GCaMP6s',...
-                'bl_prctile',87);
+                'bl_prctile',bl_prctile);
     
     df_f = segmentout.df_f;
-    spikes_caiman = extractSpikes( df_f, params.spkExtract );        
-
-    for i = 1:184
-        df_f_up(i,:) = upsample( df_f(i,:),5 );
-    end
-    spikes_caiman_up = extractSpikes( df_f_up, params.spkExtract );
+    spikes = extractSpikes( df_f, params.spkExtract );        
+    output.spikes = spikes;
+    output.params = params.spkExtract;
+%     for i = 1:184
+%         df_f_up(i,:) = upsample( df_f(i,:),5 );
+%     end
+%     spikes_caiman_up = extractSpikes( df_f_up, params.spkExtract );
     
-    save([grp_sdir mouseid '_' expname '_ref' reffile '_spikes_caiman.mat'],'spikes_caiman')
-    save([grp_sdir mouseid '_' expname '_ref' reffile '_spikes_caiman_up.mat'],'spikes_caiman_up')
+    save([grp_sdir 'noFISSA/bl_prctile' num2str(bl_prctile) '/' mouseid '_' expname '_ref' reffile '_spikes_caiman.mat'],'-struct','output')
     disp(toc)
 catch
     toc
 end
 
 % later check
-for i = 1:10
-    figure;
-    subplot(511); plot(df_f(i,:)); ylabel('df/f');
-    subplot(512); plot(spikes_caiman(i,:)); ylabel('spikes');
-%     subplot(513); plot(df_f_up(i,:)); ylabel('df/f (up)');
-%     subplot(514); plot(spikes_caiman_up(i,:)); ylabel('spikes (up)');
-%     subplot(515); plot(downsample(spikes_caiman_up(i,:),5)); ylabel('spikes (up-down)');
-end
+% for i = 1:10
+%     figure;
+%     subplot(511); plot(df_f(i,:)); ylabel('df/f');
+%     subplot(512); plot(spikes_caiman(i,:)); ylabel('spikes');
+% %     subplot(513); plot(df_f_up(i,:)); ylabel('df/f (up)');
+% %     subplot(514); plot(spikes_caiman_up(i,:)); ylabel('spikes (up)');
+% %     subplot(515); plot(downsample(spikes_caiman_up(i,:),5)); ylabel('spikes (up-down)');
+% end
